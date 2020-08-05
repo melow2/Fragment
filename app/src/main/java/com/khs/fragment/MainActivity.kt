96c9923324557d70ac80929bd9aa92b5f095f768
happiness <melow2@naver.com>
2020-08-05 14:50:48 +0900

    initial commit

--- app/src/main/java/com/khs/fragment/MainActivity.kt
+++ app/src/main/java/com/khs/fragment/MainActivity.kt
@@ -2,10 +2,16 @@ package com.khs.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.khs.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.flt_container,TestFragment.newInstance("1","2"),"TEST_FRAGMENT").commit()
     }
 }
